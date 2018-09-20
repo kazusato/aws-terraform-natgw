754de4ff231f81415a4e39d0ab4f71971d731678
Kazuhiko Sato <super80@gmail.com>
2018-09-21 05:38:39 +0900

    fixed typo

--- 01_ec2/04_igw_natgw.tf
+++ 01_ec2/04_igw_natgw.tf
@@ -2,7 +2,7 @@ resource "aws_internet_gateway" "igw_main" {
   vpc_id = "${aws_vpc.vpc_main.id}"
   tags {
     env = "${var.env}"
-    Name = "igw-nagtw-work"
+    Name = "igw-natgw-work"
   }
 }
 
